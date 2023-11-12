72dd6e5fb258a19ffdd73d2c05919caa491381a3
WKmedia <WKmedia@users.noreply.github.com>
2023-11-12 12:52:19 -0500

    updated tabs link

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 // index.js
 import StartModule from './startModule';
+// import BurgerModule from './burgerModule';
 
 document.addEventListener('DOMContentLoaded', function () {
     StartModule.component();
+    
 });
+// document.addEventListener('DOMContentLoaded', function () {
+//     BurgerModule.component();
+// });
--- src/startModule.js
+++ src/startModule.js
@@ -4,6 +4,7 @@ import Logo from './logo.jpg';
 import printMe from './print.js';
 
 
+
 if (process.env.NODE_ENV !== 'production') {
     console.log('Looks like we are in development mode!');
   }
@@ -11,11 +12,9 @@ if (process.env.NODE_ENV !== 'production') {
 
 const StartModule = (function() 
 {
-  function showTab(tabName) {
-    // Implement your logic for showing tabs
-    console.log(`Showing ${tabName}`);
-}
+ 
 function component() {
+    const elementContainer = document.createElement('div');
     const element = document.createElement('div');
     const menuDiv = document.createElement('div');//nav container
     const menuNav = document.createElement('ul');//nav menu
@@ -23,26 +22,49 @@ function component() {
     const item2 = document.createElement('li');//nav list item
     const item3 = document.createElement('li');//nav list item
     const item4 = document.createElement('li');//nav list item
+    const tab1 = document.createElement('div');
+    const tab2 = document.createElement('div');
+    const tab3 = document.createElement('div');
+    const tab4 = document.createElement('div');   
     const mainHeading = document.createElement('h1');
     const subHeading = document.createElement('p');
     const wonderfulText = document.createElement('p');
     const btn = document.createElement('button');
     const myLogo = new Image();
     const myImage = new Image();
+
+
     
     
     
     element.classList.add('hello');
-    btn.onclick = printMe;
-    menuDiv.setAttribute('id', 'meniDiv');
+    
+    mainHeading.setAttribute('class', 'h1style'); 
+    subHeading.setAttribute('class', 'pstyle1'); 
+    wonderfulText.setAttribute('class', 'pstyle2' );
+    btn.setAttribute('class', 'joinBtn');
+    btn.onclick = printMe;        
+    menuDiv.setAttribute('id', 'menuDiv');
+    myLogo.setAttribute('id', 'my-logo');
     menuNav.setAttribute('id', 'menuNav');
+    menuNav.setAttribute('class', 'tabs');
     item1.setAttribute('class', 'tab');
     item2.setAttribute('class', 'tab');
     item3.setAttribute('class', 'tab');
     item4.setAttribute('class', 'tab');
+    tab1.setAttribute('id', 'tab1');
+    tab1.setAttribute('class', 'tab-content');
+    tab2.setAttribute('id', 'tab2');
+    tab2.setAttribute('class', 'tab-content');
+    tab3.setAttribute('id', 'tab3');
+    tab3.setAttribute('class', 'tab-content');
+    tab4.setAttribute('id', 'tab4');
+    tab4.setAttribute('class', 'tab-content');
+
     item1.addEventListener('click', function() {
       showTab('tab1')
     });
+    
     item2.addEventListener('click', function() {
       showTab('tab2')
     });
@@ -50,25 +72,23 @@ function component() {
       showTab('tab3')
     });
     item4.addEventListener('click', function() {
-      showTab('tab3')
+      showTab('tab4')
     });
-    mainHeading.setAttribute('class', 'h1style'); 
-    subHeading.setAttribute('class', 'pstyle1'); 
-    wonderfulText.setAttribute('class', 'pstyle2' );
-    btn.setAttribute('class', 'joinBtn');        
-    menuDiv.setAttribute('id', 'menuDiv');
+    
     
 
     
     // menuDiv.textContent = 'Menu Container';
     myLogo.src = Logo;
-    myLogo.setAttribute('id', 'my-logo');
+    
     // menuNav.textContent = 'menu Nav';
 
     item1.textContent = 'Burger\'s';
     item2.textContent = 'Steak\'s';
     item3.textContent = 'Side\'s';
     item4.textContent = 'Contact\'s';
+
+    
 
     element.innerHTML = ('Hello there!');
     btn.innerHTML = 'Click me and check the console!';   
@@ -89,14 +109,40 @@ function component() {
     menuNav.append(item2);
     menuNav.append(item3);
     menuNav.append(item4);
+    element.append(tab1);
+    element.append(tab2);
+    element.append(tab3);
+    element.append(tab4);
+    
     element.append(mainHeading);
     element.append(subHeading);
     element.append(wonderfulText);   
     element.appendChild(btn);
     console.log('this works');
 
-    document.body.appendChild(component());
+
+    document.body.appendChild(element);
   }
+
+  function showTab(tabId) {
+    // Implement your logic for showing tabs
+    const tabs = document.querySelectorAll('.tab');
+            tabs.forEach(tab => tab.classList.remove('active'));
+
+            const tabContents = document.querySelectorAll('.tab-content');
+            tabContents.forEach(content => content.classList.remove('active'));
+
+            const selectedTab = document.getElementById(tabId);
+            selectedTab.classList.add('active');
+
+            const selectedContent = document.getElementById(tabId);
+            selectedContent.classList.add('active');
+
+            
+    console.log(`Showing ${tabId}`);
+}
+ 
+
     return {
       component:component,
     };
