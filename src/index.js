b7eab67c02d9dc5a9f34b4e8050e503a3119f2d3
WKmedia <WKmedia@users.noreply.github.com>
2023-11-11 22:47:20 -0500

    added  and heading, subheading, logo and menu

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.css';
-import Icon from './icon.png';
+import backImage from './backImageSm.jpg';
+import Logo from './logo.jpg';
 import printMe from './print.js';
 
 
@@ -10,23 +11,74 @@ if (process.env.NODE_ENV !== 'production') {
 
 function component() {
     const element = document.createElement('div');
+    const menuDiv = document.createElement('div');//nav container
+    const menuNav = document.createElement('ul');//nav menu
+    const item1 = document.createElement('li');//nav list item
+    const item2 = document.createElement('li');//nav list item
+    const item3 = document.createElement('li');//nav list item
+    const mainHeading = document.createElement('h1');
+    const subHeading = document.createElement('p');
+    const wonderfulText = document.createElement('p');
     const btn = document.createElement('button');
-    const myIcon = new Image();
-       
-        
-    // Lodash, now imported by this script
-    element.innerHTML = ('Hello webpack');
-    element.classList.add('hello');
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-    myIcon.src = Icon;
+    const myLogo = new Image();
+    const myImage = new Image();
     
+    
+    
+    element.classList.add('hello');
+    btn.onclick = printMe;
+    menuDiv.setAttribute('id', 'meniDiv');
+    menuNav.setAttribute('id', 'menuNav');
+    item1.setAttribute('class', 'tab');
+    item2.setAttribute('class', 'tab');
+    item3.setAttribute('class', 'tab');
+    item1.addEventListener('click', function() {
+      showTab('tab1')
+    })
+    item2.addEventListener('click', function() {
+      showTab('tab2')
+    })
+    item3.addEventListener('click', function() {
+      showTab('tab3')
+    })
+    mainHeading.setAttribute('class', 'h1style'); 
+    subHeading.setAttribute('class', 'pstyle1'); 
+    wonderfulText.setAttribute('class', 'pstyle2' );
+    btn.setAttribute('class', 'joinBtn');        
+    menuDiv.setAttribute('id', 'menuDiv');
+    
+
+    
+    // menuDiv.textContent = 'Menu Container';
+    myLogo.src = Logo;
+    myLogo.setAttribute('id', 'my-logo');
+    // menuNav.textContent = 'menu Nav';
+
+    item1.textContent = 'Burger\'s';
+    item2.textContent = 'Steak\'s';
+    item3.textContent = 'Side\'s';
+
+    element.innerHTML = ('Hello there!');
+    btn.innerHTML = 'Click me and check the console!';   
+    myImage.src = backImage;   
+    mainHeading.textContent = 'Join us for an extraordinary culinary journey.';    
+    subHeading.textContent = 'Book your table today and savor the magic of Our Wonderful Restaurant.';    
+    wonderfulText.textContent = 'Whether you\'re here for a casual meal with friends or a special celebration, Our Wonderful Restaurant promises an unforgettable dining experience.';
+   
   
    
-   
-   
-   
-    element.appendChild(myIcon);  
+
+
+
+    element.prepend(menuDiv);
+    menuDiv.prepend(myLogo);
+    menuDiv.append(menuNav);
+    menuNav.append(item1);
+    menuNav.append(item2);
+    menuNav.append(item3);
+    element.append(mainHeading);
+    element.append(subHeading);
+    element.append(wonderfulText);   
     element.appendChild(btn);
     console.log('this works');
 
--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: {
-    app: './src/index.js',
+    index: './src/index.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
